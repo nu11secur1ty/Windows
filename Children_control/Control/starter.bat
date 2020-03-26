948dd442201121595ad9ee8ed37b29f22b632463
nu11secur1ty <venvaropt@gmail.com>
2020-03-26 21:27:45 +0200

    Create starter.bat

---
+++ Children_control/Control/starter.bat
@@ -0,0 +1,5 @@
+@echo of
+:: Change your username with your username
+:: If you need you must changing your path of Control folder
+cd C:\Users\your_username\Desktop\Control
+start_control.vbs control_network_30_minutes.bat

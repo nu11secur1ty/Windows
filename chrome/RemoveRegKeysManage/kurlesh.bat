ace6f11ac2cb698be7881f56ef0957f8d4ac3f35
nu11secur1ty <venvaropt@gmail.com>
2020-04-08 21:24:56 +0300

    Create kurlesh.bat

---
+++ chrome/RemoveRegKeysManage/kurlesh.bat
@@ -0,0 +1,4 @@
+@echo off
+:@nu11secur1ty
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome /v ChromeCleanupEnabled
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome /v ChromeCleanupReportingEnabled

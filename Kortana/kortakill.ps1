a40d6bfd3da75243169805b3b3c96be3a0d4d0c6
nu11secur1ty <venvaropt@gmail.com>
2020-09-14 14:03:39 +0300

    Update kortakill.ps1

--- Kortana/kortakill.ps1
+++ Kortana/kortakill.ps1
@@ -1,1 +1,3 @@
+cmd /c echo y | powershell "read-host" Set-ExecutionPolicy RemoteSigned
 Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage
+cmd /c echo y | powershell "read-host" Set-ExecutionPolicy Restricted

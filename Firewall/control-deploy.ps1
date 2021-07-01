695bb6398932ef1b0d9857368f47aedc35e9585c
nu11secur1ty <venvaropt@gmail.com>
2021-07-01 16:03:11 +0300

    Update control-deploy.ps1

--- Firewall/control-deploy.ps1
+++ Firewall/control-deploy.ps1
@@ -1,3 +1,5 @@
-Set-ExecutionPolicy RemoteSigned
-Set-ExecutionPolicy unrestricted
-Set-ExecutionPolicy restricted
+# Set-ExecutionPolicy RemoteSigned
+# Set-ExecutionPolicy unrestricted
+# Set-ExecutionPolicy restricted
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force;

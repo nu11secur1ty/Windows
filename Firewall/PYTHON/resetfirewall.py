89607ddbb4212fca478bd7ffbffcfc0a5a0ee76c
nu11secur1ty <venvaropt@gmail.com>
2021-07-28 12:28:39 +0300

    Update resetfirewall.py

--- Firewall/PYTHON/resetfirewall.py
+++ Firewall/PYTHON/resetfirewall.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 import os
-os.system('echo # Copyright (c) 1993-2009 Microsoft Corp." > C:\Windows\System32\drivers\etc\hosts')
-os.system('echo # Copyright (c) 2021 nu11secur1ty" >> C:\Windows\System32\drivers\etc\hosts')
+os.system("echo # Copyright (c) 1993-2009 Microsoft Corp. > C:\Windows\System32\drivers\etc\hosts")
+os.system("echo # Copyright (c) 2021 nu11secur1ty >> C:\Windows\System32\drivers\etc\hosts")

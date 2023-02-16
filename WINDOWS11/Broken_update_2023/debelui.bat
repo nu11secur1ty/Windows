327a7199ad04663bb5866afcb298b13227bee18e
nu11secur1ty <venvaropt@gmail.com>
2023-02-16 17:00:24 +0200

    Update debelui.bat

--- WINDOWS11/Broken_update_2023/debelui.bat
+++ WINDOWS11/Broken_update_2023/debelui.bat
@@ -8,7 +8,7 @@ ren C:\Windows\System32\catroot2 Catroot2.old
 net start wuauserv 
 net start cryptSvc 
 net start bits 
+netsh winsock reset
 rmdir /s /q C:\Windows\SoftwareDistribution.old 
 rmdir /s /q C:\Windows\System32\Catroot2.old
-netsh winsock reset
 shutdown /R

e56c75158e88fb63681f4181e4ddd447e9b803bf
nu11secur1ty <venvaropt@gmail.com>
2025-05-29 14:05:36 +0300

    Add files via upload

---
+++ TEMP/tmp.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+del /s /f /q C:\Windows\Temp\*.*
+del /s /f /q C:\Windows\SoftwareDistribution\Downloads\*.*
+del /s /f /q C:\Windows\Prefetch\*.*
